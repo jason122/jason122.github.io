f28d60289f0ecf80593136851a1f9fa334ac6f4f
atebitcake <jasonrlevine02@gmail.com>
2017-12-16 04:49:12 +0000

    added kepress event to thatwaseasy button

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -1,8 +1,14 @@
-
-
 function sayEasy() {
     var thatWasEasy = new Audio("that_was_easy.mp3");
     thatWasEasy.play();
 }
 
+function delgKeyPress(event) {
+    if (event.charCode == 111) {
+        $("#easy").trigger("click");
+    }
+}
+
 $("#easy").on("click", sayEasy);
+
+$(document).keypress(delgKeyPress);
